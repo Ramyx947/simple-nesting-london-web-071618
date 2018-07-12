4b01d4a0930a31e4f1bcc41026f5d88c8b257d71
Ramona M <r1mari@yahoo.com>
2018-07-12 08:23:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,15 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
     #alan_new_info = "GUI"
      alan_kay=programmer_hash[:alan_kay][:known_for] = alans_new_info
-     programmer_hash.each do |attribute, value|
+     programmer_hash.each do |person, data|
+       data.each do |attribute, value|
+         if attribute == :known_for
+           value.each do |info|
+             puts "#{flavor}"
+           end
+         end
+       end
+     end 
     
   
      
